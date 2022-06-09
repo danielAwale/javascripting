52d6623de1450520cfd158f207f044fdbe789579
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-09 06:05:52 +0000

    every word 1 second delay need to refactor after

--- async/hello_timeout.js
+++ async/hello_timeout.js
@@ -1,6 +1,14 @@
 
 setTimeout(() => {
-  console.log('4311o th3r3 w0r1d');
-}, 3000);
+  console.log('4311o');
+}, 1000);
+
+setTimeout(() => {
+  console.log('th3r3');
+}, 1000);
+
+setTimeout(() => {
+  console.log('w0r1d');
+}, 1000);
 
 
